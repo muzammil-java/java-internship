828920815c134d8a3fce533e2f4a7e85d3fbbb2c
muzammil-java <muzammil2360@gmail.com>
2024-07-31 20:31:19 +0500

    Update calculator

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -3,34 +3,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner myobj = new Scanner(System.in);
-        System.out.println( " Enter char +,-,*,/ that you want to perform operations ");
-        char operator = myobj.next().charAt(0);
-            System.out.println( " Enter value 1 ");
-        float num1 = myobj.nextFloat();
-        System.out.println( " Value 1 on which you perform operator ");
-            System.out.println( " Enter value 2 ");
-        float num2 = myobj.nextFloat();
-             System.out.println( " Value 2 on which you perform operator ");
+        System.out.println( " Enter Character that you want to perform operations.");
+        System.out.println("( + ) Addition.");
+        System.out.println("( - ) Subtraction.");
+        System.out.println("( * ) Multiplication.");
+        System.out.println("( / ) Division.");
+        System.out.println("( 0 ) Exit.");
 
-        switch(operator){
-        case '+':
-            System.out.println( " The Addition of " + num1 + " and " + num2 + " is " + (add(num1, num2)) );
-            break;
-        case '-':
-            System.out.println( " The Subtraction of " + num1 + " and " + num2 + " is " + (sub(num1,num2)) );
-            sub(num1, num2);
-            break;
-        case '*':
-            System.out.println( " The Multiplication of " + num1 + " and " + num2 + " is " + (mul(num1,num2)) );
-            break;
-        case '/':
-            if(num2 != 0)
-                System.out.println( " The Division of " + num1 + " and " + num2 + " is " + (div(num1,num2)) );
-            else
-                System.out.println( " Cannot divided by zero " );
-            break;
-            default:
-             System.out.println( " Invalid operator  " );
+        char operator = myobj.next().charAt(0);
+
+        while(operator != '0') {
+
+            System.out.println(" Enter value 1 ");
+            float num1 = myobj.nextFloat();
+            System.out.println(" Enter value 2 ");
+            float num2 = myobj.nextFloat();
+
+            switch (operator) {
+                case '+':
+                    System.out.println(" The Addition of " + num1 + " and " + num2 + " is " + (add(num1, num2)));
+                    break;
+                case '-':
+                    System.out.println(" The Subtraction of " + num1 + " and " + num2 + " is " + (sub(num1, num2)));
+                    sub(num1, num2);
+                    break;
+                case '*':
+                    System.out.println(" The Multiplication of " + num1 + " and " + num2 + " is " + (mul(num1, num2)));
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        System.out.println(" The Division of " + num1 + " and " + num2 + " is " + (div(num1, num2)));
+                    else
+                        System.out.println(" Cannot divided by zero ");
+                    break;
+                case '0':
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                default:
+                    System.out.println(" Invalid operator  ");
+
+            }
+
+            System.out.println( " Enter Character that you want to perform operations.");
+            System.out.println("( + ) Addition.");
+            System.out.println("( - ) Subtraction.");
+            System.out.println("( * ) Multiplication.");
+            System.out.println("( / ) Division.");
+            System.out.println("( 0 ) Exit.");
+
+            operator = myobj.next().charAt(0);
 
         }
     }
